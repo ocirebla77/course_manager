7e328e583ba38e58a736eeb604368ac59ff00705
ocirebla77 <ocirebla@hotmail.com>
2022-06-21 23:29:08 -0300

    first template html

--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -1,12 +1,7 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-course-list',
-  template: '<h2>Course List</h2>',
- 
+  templateUrl: './course-list.component.html',
 })
-
-
-export class CourseListComponent{
-
-}
+export class CourseListComponent {}
